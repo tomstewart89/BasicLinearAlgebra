7dda350d50e45a5ef83961bd42892c4bf7149d18
tomstewart89 <tomstewart3107@gmail.com>
2021-07-26 21:49:51 +0900

    Use permutation matrix instead of idx in LU functions

--- test/test_linear_algebra.cpp
+++ test/test_linear_algebra.cpp
@@ -1,101 +1,81 @@
-#include <gtest/gtest.h>
 #include "../BasicLinearAlgebra.h"
+#include <gtest/gtest.h>
 
 using namespace BLA;
 
-TEST(LinearAlgebra, LUDecomposition)
-{
-    Matrix<3, 3> A{2, 5, 8, 0, 8, 6, 6, 7, 5};
+TEST(LinearAlgebra, LUDecomposition) {
+  Matrix<3, 3> A{2, 5, 8, 0, 8, 6, 6, 7, 5};
 
-    auto LU = A;
-    ArrayMatrix<3, 1, int> P;
+  auto LU = A;
+  ArrayMatrix<3, 1, int> idx;
 
-    LUDecompose(LU, P);
-    Matrix<3, 3> L, U;
+  PermutationMatrix<3> P;
 
-    for (int i = 0; i < 3; ++i)
-    {
-        for (int j = 0; j < 3; ++j)
-        {
-            // Fill in the diagonal
-            if (i == j)
-            {
-                L(i, j) = 1.0;
-                U(i, j) = LU(i, j);
-            }
-            // Upper triangle
-            else if (i < j)
-            {
-                L(i, j) = 0.0;
-                U(i, j) = LU(i, j);
-            }
-            // Lower triangle
-            else
-            {
-                L(i, j) = LU(i, j);
-                U(i, j) = 0.0;
-            }
-        }
+  LUDecompose(LU, P);
+
+  Matrix<3, 3> L, U;
+  L = U = Zeros<3, 3, float>();
+
+  for (int i = 0; i < 3; ++i) {
+    L(i, i) = 1.0;
+    U(i, i) = LU(i, i);
+
+    for (int j = i + 1; j < 3; ++j) {
+      U(i, j) = LU(i, j);
+      L(j, i) = LU(j, i);
     }
+  }
 
-    auto A_reconstructed = L * U;
+  auto A_reconstructed = P * L * U;
 
-    for (int i = 0; i < 3; ++i)
-    {
-        for (int j = 0; j < 3; ++j)
-        {
-            EXPECT_FLOAT_EQ(A_reconstructed(P(i), j), A(i, j));
-        }
+  for (int i = 0; i < 3; ++i) {
+    for (int j = 0; j < 3; ++j) {
+      EXPECT_FLOAT_EQ(A_reconstructed(P(i), j), A(i, j));
     }
+  }
 }
 
-TEST(LinearAlgebra, LUSolution)
-{
-    Matrix<3, 3> A{2, 5, 8, 0, 8, 6, 6, 7, 5};
-    Matrix<3, 1> b{10, 11, 12};
-    Matrix<3, 1> x;
-    Matrix<3, 1> x_expected = {0.41826923, 0.97115385, 0.53846154};
+TEST(LinearAlgebra, LUSolution) {
+  Matrix<3, 3> A{2, 5, 8, 0, 8, 6, 6, 7, 5};
+  Matrix<3, 1> b{10, 11, 12};
+  Matrix<3, 1> x;
+  Matrix<3, 1> x_expected = {0.41826923, 0.97115385, 0.53846154};
 
-    ArrayMatrix<3, 1, int> P;
+  ArrayMatrix<3, 1, int> P;
 
-    LUDecompose(A, P);
+  LUDecompose(A, P);
 
-    LUSolve(A, P, b, x);
+  LUSolve(A, P, b, x);
 
-    for (int i = 0; i < 3; ++i)
-    {
-        EXPECT_FLOAT_EQ(x_expected(i), x(i));
+  for (int i = 0; i < 3; ++i) {
+    EXPECT_FLOAT_EQ(x_expected(i), x(i));
+  }
+}
+
+TEST(LinearAlgebra, Inversion) {
+  Matrix<2, 2> A{1, 2, 3, 4};
+  Matrix<2, 2> A_inv_numpy{-2, 1, 1.5, -0.5};
+
+  auto A_inv = A;
+  Invert(A_inv);
+
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      EXPECT_FLOAT_EQ(A_inv_numpy(i, j), A_inv(i, j));
     }
+  }
 }
 
-TEST(LinearAlgebra, Inversion)
-{
-    Matrix<2, 2> A{1, 2, 3, 4};
-    Matrix<2, 2> A_inv_numpy{-2, 1, 1.5, -0.5};
+TEST(Arithmetic, Determinant) {
+  Matrix<3, 3> B = {0.13397823, 0.21299481, 0.34636886, 0.75536007, 0.98363649,
+                    0.59470267, 0.71449196, 0.08585212, 0.55099433};
 
-    auto A_inv = A;
-    Invert(A_inv);
+  float det_numpy = -0.15333813;
 
-    for (int i = 0; i < 2; ++i)
-    {
-        for (int j = 0; j < 2; ++j)
-        {
-            EXPECT_FLOAT_EQ(A_inv_numpy(i, j), A_inv(i, j));
-        }
-    }
+  EXPECT_FLOAT_EQ(Determinant(B), det_numpy);
 }
 
-TEST(Arithmetic, Determinant)
-{
-    Matrix<3, 3> B = {0.13397823, 0.21299481, 0.34636886, 0.75536007, 0.98363649, 0.59470267, 0.71449196, 0.08585212, 0.55099433};
-
-    float det_numpy = -0.15333813;
-
-    EXPECT_FLOAT_EQ(Determinant(B), det_numpy);
-}
-
-int main(int argc, char **argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
