3f86f7b7eaeac91e1acc190556ecd0af1718a243
Tom Stewart <tomstewart3107@gmail.com>
2021-07-30 20:59:49 +0900

    Update examples

--- examples/SolveLinearEquations/SolveLinearEquations.ino
+++ examples/SolveLinearEquations/SolveLinearEquations.ino
@@ -4,6 +4,10 @@ using namespace BLA;
 
 void setup()
 {
+    // One very common thing that you might like to do with matrices is to solve systems
+    // of linear equations of the form: A * x = b
+
+    // 
 }
 
 void loop() {}
